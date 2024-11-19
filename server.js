03107df5e7d4ce46bc1bff4bcf3d90973ce538cc
Sophie Van Schil <sophievanschil@hotmail.com>
2024-11-19 22:27:44 +0100

    google4

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
+
 // Set up session middleware (no duplicate declaration)
 app.use(session({
   secret: process.env.SESSION_SECRET, 
